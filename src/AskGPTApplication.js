011bb64d7a143a1541a1e54a69b1a1d279371059
Kota Mizushima <mizukota@gmail.com>
2022-12-11 04:14:23 +0900

    Make Web Application to use OpenAI GPT-3 model

--- src/AskGPTApplication.js
+++ src/AskGPTApplication.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import { Button } from "@mui/material";
 import TextareaAutosize from '@mui/base/TextareaAutosize';
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: "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"
+});
+const openai = new OpenAIApi(configuration);
+
+async function generate(prompt, setGeneratedText) {
+  console.log(prompt);
+  const completion = await openai.createCompletion({
+    model: "text-davinci-001",
+    prompt: prompt,
+    temperature: 0.6,
+    max_tokens: 2000
+  });
+  const text = completion.data.choices[0].text;
+  console.log(text);
+  setGeneratedText(text);
+}
 function App() {
+  const [promptText, setPromptText] = useState("")
+  const [generatedText, setGeneratedText] = useState("")
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,17 +31,18 @@ function App() {
         maxRows={4}
         aria-label="maximum height"
         placeholder="プロンプトを入力してください"
-        defaultValue=""
+        onChange={e => setPromptText(e.target.value)}
+        value={promptText}
         style={{ height: 100, width: 400 }}
       />
       </header>
-      <Button variant="outlined">テキスト生成</Button>
+      <Button variant="outlined" onClick={() => generate(promptText, setGeneratedText)}>テキスト生成</Button>
       <p>生成結果</p>
       <TextareaAutosize
         maxRows={4}
         aria-label="maximum height"
         placeholder="生成されたテキストが出力されます"
-        defaultValue=""
+        value={generatedText}
         style={{ height: 100, width: 400 }}
       />
     </div>
